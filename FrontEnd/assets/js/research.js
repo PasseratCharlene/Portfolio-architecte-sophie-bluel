69891f24d5de5f00f124347a10862315399ab8bb
PasseratCharlene <passeratcharlene13@gmail.com>
2024-05-12 10:14:02 +0200

    login html css

--- FrontEnd/assets/js/research.js
+++ FrontEnd/assets/js/research.js
@@ -4,15 +4,15 @@ async function displayResearch (){
 const data = await retriveContent("http://localhost:5678/api/categories")
     console.log(data)
     const research = document.querySelector(".research")
-    research.innerHTML = `<button data-id="0">Tous</button>`
+    research.innerHTML = `<button data-categoryid="0">Tous</button>`
     data.forEach(element => {
-      research.innerHTML += `<button data-id="${element.id}">${element.name}</button>`
+      research.innerHTML += `<button data-categoryid="${element.id}">${element.name}</button>`
       })
       const btns = document.querySelectorAll(".research button")
       const arrayBtns = Array.from(btns)
       arrayBtns.forEach(btn => {
         btn.addEventListener("click", e =>{
-          const categoryId = e.target.dataset.id
+          const categoryId = e.target.dataset.categoryid
           displayGallery(parseInt(categoryId))
         })
       })
